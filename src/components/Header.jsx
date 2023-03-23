29cf3f130d74b9d9afe8343a6c43abe7de0cdd5f
David Olowomeye <olowomeyedavid242@gmail.com>
2023-03-23 02:05:16 +0100

    Did the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import style from "../styles/Header.module.css";
+import Logo from "../assets/Logo.png";
+import Link from "next/link";
+import { GiShoppingCart } from "react-icons/gi";
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <header className={style.header}>
+      <div className={style.logo}>
+        <Image src={Logo} alt="Logo" width={50} height={50} />
+        <span>FUDO</span>
+      </div>
 
-export default Header
+      <ul className={style.menu}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/menu">Menu</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+
+      <div className={style.navIcon}>
+        <div className={style.cartIcon}>
+          <GiShoppingCart size={35} color="#2e2e2e" />
+          <div className={style.badge}>1</div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
